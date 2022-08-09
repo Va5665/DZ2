e37abe8f197a8fbc316e5532d690389c00bf6e37
Vasiliy Karavaev <vasiliykaravaev@gmail.com>
2022-08-09 17:22:53 +0530

    добаваили

---
+++ 3/Program.cs
@@ -0,0 +1,22 @@
+﻿Дмитрий Винниченко, [09.08.2022 17:20]
+int numberA = new Random().Next(1, 100);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 100);
+Console.WriteLine(numberB);
+int numberC = new Random().Next(1, 100);
+Console.WriteLine(numberC);
+int max = numberA;
+if (numberB > numberA) 
+{
+    max = numberB;
+}
+else
+{
+    max = numberA;
+}
+if (numberC > max)
+{
+    max = numberC;
+}
+Console.WriteLine(max);
+
